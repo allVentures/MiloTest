438826327d956b7e1ba0cd55138ddedd1563485a
allVentures <paul@allphotobangkok.com>
2018-12-21 14:54:53 +0100

    user details

--- MiloApp/views.py
+++ MiloApp/views.py
@@ -9,11 +9,26 @@ from MiloApp.models import User
 class MainPage(View):
     def get(self, request):
         all_users = User.objects.all().order_by('-birthdate')
-        users_age = []
         for usr in all_users:
             today = date.today()
             usr.age = round((today - usr.birthdate).days / 365)
+            if usr.random_number % 5 == 0 and usr.random_number % 3 == 0:
+                usr.bizz = 53
+            elif usr.random_number % 5 == 0:
+                usr.bizz = 5
+            elif usr.random_number % 3 == 0:
+                usr.bizz = 3
+            else:
+                usr.bizz = 0
 
-        ctx = {"users": all_users }
-
+        ctx = {"users": all_users}
         return render(request, "main_page.html", ctx)
+
+
+class ShowUserDetails(View):
+    def get(self, request, id):
+        user_data = User.objects.get(id=id)
+
+        ctx = {"user_data": user_data}
+
+        return render(request, "show_user_details.html", ctx)
